d4453ca03d22ba98c9d6bc269a9bcf6c5492fbdd
UNDO <71146012+FreeBetSocial@users.noreply.github.com>
2022-06-13 00:28:46 +0300

    Доска почёта

--- src/components/DashBoard/dash.jsx
+++ src/components/DashBoard/dash.jsx
@@ -1,21 +1,66 @@
 import classes from "./dash.module.scss"
-import img1 from "../../img/avatar_profile.png"
+import ava from "../../img/avatar_profile.png"
+
+
+const items = [
+    {
+        img:ava,
+        name:"sdffsd",
+        count:0
+    },
+    {
+        img:ava,
+        name:"sdffsd",
+        count:0
+    },
+    {
+        img:ava,
+        name:"sdffsd",
+        count:0
+    },
+    {
+        img:ava,
+        name:"sdffsd",
+        count:0
+    }
+    ,{
+        img:ava,
+        name:"sdffsd",
+        count:0
+    }
+    ,{
+        img:ava,
+        name:"sdffsd",
+        count:0
+    }
+    ,{
+        img:ava,
+        name:"sdffsd",
+        count:0
+    }
+    ,{
+        img:ava,
+        name:"sdffsd",
+        count:0
+    }
+]
 
 const Dash =()=>{
     return (
         <div className={"content"}>
             <div className={classes.Dash}>
                 <h1>Доска почета</h1>
-                    <div className={classes.row_dash}>
-                        <div className={classes.banner6}>
-                            <div className={classes.col_dash}>
-                               <img src={img1}/> 
-                            </div>
-                            <div className={classes.col_dash}>
-                               <img src={img1}/> 
-                            </div>
+                <div className={classes.items}>
+                    {items.map((e,i)=>(
+                        <div>
+                            <img src={e.img}/>
+                            <h3>{e.name}</h3>
+                            <p>Посещено мероприятий:</p>
+                            <p data-value>{e.count}</p>
+                            <div>{i+1} место</div>
                         </div>
-                    </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
